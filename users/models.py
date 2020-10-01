b28d79cf559facb2c1ce4c8dfd056d8b277e9939
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-01 15:47:28 +0900

    #1 AIRBNB-CLONE ~#12.3
    #12 FBV

---
+++ users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "Usd"), (CURRENCY_KRW, "Krw"))
+
+    avatar = models.ImageField(upload_to="avatars", blank=True)  # image
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(
+        blank=True
+    )  # db column 이 추가되면 row에 영향을 주게 됨. 그래서 값이 없어도 default로 비어있는 string을 주는 것.
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
