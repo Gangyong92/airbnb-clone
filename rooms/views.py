82af51686018bf28a90f668e0f453507de338ca2
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-02 21:56:45 +0900

    #5 AIRBNB-CLONE ~#13.6
    s_amenities, s_facilities filter 개선

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.db.models import Q
 from django_countries import countries
 from . import models
 
@@ -32,8 +33,8 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -50,7 +51,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -65,11 +66,61 @@ def search(request):
         "facilities": facilities,
     }
 
+    filter_args = {}
+
+    # city filter
+    if city != "Anywhere":
+        # startswith 옵셥
+        filter_args["city__startswith"] = city
+
+    # country filter
+    filter_args["country"] = country
+
+    # room_type filter
+    if room_type != 0:
+        # room_type는 RoomType의 pk를 받고 있고 Room에서 room_type은 fk를 통해 접근하고 있음
+        filter_args["room_type__pk"] = room_type
+
+    # price filter
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    # guests, bedrooms, beds, baths filter
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    # instant, superhost filter
+    if instant is True:
+        filter_args["instant_book"] = True
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    if len(s_amenities) > 0:
+        s_amenities_query = Q(amenities__pk=int(s_amenities[0]))
+        for s_amenity in s_amenities[1:]:
+            s_amenities_query |= Q(amenities__pk=int(s_amenity))
+        rooms = rooms.filter(s_amenities_query).distinct()
+
+    if len(s_facilities) > 0:
+        s_facilities_query = Q(facilities__pk=int(s_facilities[0]))
+        for s_facility in s_facilities[1:]:
+            s_facilities_query |= Q(facilities__pk=int(s_facility))
+        rooms = rooms.filter(s_facilities_query).distinct()
+
     return render(
         request,
         "rooms/search.html",
         context={
             **form,
             **choices,
+            "rooms": rooms,
         },
     )
