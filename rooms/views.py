2e6c8521875f325b98ec99f903e21f4b549a1444
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-03 17:14:15 +0900

    #6 AIRBNB-CLONE ~#13.10
    search page form으로 구현, paginator 구현

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
 from django.db.models import Q
-from django_countries import countries
-from . import models
+from django.core.paginator import Paginator
+from . import models, forms
 
 
 class HomeView(ListView):
@@ -23,104 +23,104 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-def search(request):
-    city = request.GET.get("city", "Anywhere")
-    city = str.capitalize(city)
-    country = request.GET.get("country", "KR")
-    room_type = int(request.GET.get("room_type", 0))
-    price = int(request.GET.get("price", 0))
-    guests = int(request.GET.get("guests", 0))
-    bedrooms = int(request.GET.get("bedrooms", 0))
-    beds = int(request.GET.get("beds", 0))
-    baths = int(request.GET.get("baths", 0))
-    instant = bool(request.GET.get("instant", False))
-    superhost = bool(request.GET.get("superhost", False))
-    s_amenities = request.GET.getlist("amenities")
-    s_facilities = request.GET.getlist("facilities")
+class SearchView(View):
 
-    # request에서 가져오는 것
-    form = {
-        "city": city,
-        "s_country": country,
-        "s_room_type": room_type,
-        "price": price,
-        "guests": guests,
-        "bedrooms": bedrooms,
-        "beds": beds,
-        "baths": baths,
-        "s_amenities": s_amenities,
-        "s_facilities": s_facilities,
-        "instant": instant,
-        "superhost": superhost,
-    }
+    """ SearchView Definition """
 
-    room_types = models.RoomType.objects.all()
-    amenities = models.Amenity.objects.all()
-    facilities = models.Facility.objects.all()
+    def get(self, request):
+        country = request.GET.get("country")
 
-    # DB에서 가져오는 것
-    choices = {
-        "countries": countries,
-        "room_types": room_types,
-        "amenities": amenities,
-        "facilities": facilities,
-    }
+        if country:
+            # 어떤 데이터를 가지고 있음. 무슨 데이터인지는 아직 모름
+            form = forms.SearchForm(request.GET)
 
-    filter_args = {}
+            # is_valid()에서 데이터를 확인하는 일들이 일어남.
+            if form.is_valid():
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
 
-    # city filter
-    if city != "Anywhere":
-        # startswith 옵셥
-        filter_args["city__startswith"] = city
+                filter_args = {}
 
-    # country filter
-    filter_args["country"] = country
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
 
-    # room_type filter
-    if room_type != 0:
-        # room_type는 RoomType의 pk를 받고 있고 Room에서 room_type은 fk를 통해 접근하고 있음
-        filter_args["room_type__pk"] = room_type
+                filter_args["country"] = country
 
-    # price filter
-    if price != 0:
-        filter_args["price__lte"] = price
+                # cleaned_data가 알아서 잘 정리해줘서 pk 필요 없음.
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
 
-    # guests, bedrooms, beds, baths filter
-    if guests != 0:
-        filter_args["guests__gte"] = guests
-    if bedrooms != 0:
-        filter_args["bedrooms__gte"] = bedrooms
-    if beds != 0:
-        filter_args["beds__gte"] = beds
-    if baths != 0:
-        filter_args["baths__gte"] = baths
+                if price is not None:
+                    filter_args["price__lte"] = price
 
-    # instant, superhost filter
-    if instant is True:
-        filter_args["instant_book"] = True
-    if superhost is True:
-        filter_args["host__superhost"] = True
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
 
-    rooms = models.Room.objects.filter(**filter_args)
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
 
-    if len(s_amenities) > 0:
-        s_amenities_query = Q(amenities__pk=int(s_amenities[0]))
-        for s_amenity in s_amenities[1:]:
-            s_amenities_query |= Q(amenities__pk=int(s_amenity))
-        rooms = rooms.filter(s_amenities_query).distinct()
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
 
-    if len(s_facilities) > 0:
-        s_facilities_query = Q(facilities__pk=int(s_facilities[0]))
-        for s_facility in s_facilities[1:]:
-            s_facilities_query |= Q(facilities__pk=int(s_facility))
-        rooms = rooms.filter(s_facilities_query).distinct()
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
 
-    return render(
-        request,
-        "rooms/search.html",
-        context={
-            **form,
-            **choices,
-            "rooms": rooms,
-        },
-    )
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+
+                # 앞서 했던 필터와 다르게 cleaned_data가 알아서 데이터를 정리해줘서 pk를 안씀
+                if len(amenities) > 0:
+                    amenities_query = Q(amenities=amenities[0])
+                    for amenity in amenities[1:]:
+                        amenities_query |= Q(amenities=amenity)
+                    qs = qs.filter(amenities_query).distinct()
+
+                if len(facilities) > 0:
+                    facilities_query = Q(facilities=facilities[0])
+                    for facility in facilities[1:]:
+                        facilities_query |= Q(facilities=facility)
+                    qs = qs.filter(facilities_query).distinct()
+
+                page = request.GET.get("page")
+
+                paginator = Paginator(qs, 1)
+
+                rooms = paginator.get_page(page)
+
+                # 삭제 필요한 page query 문자열 생성
+                remove_page_url = f"&page={page}"
+                query_url = request.get_full_path()[len("/rooms/search/?") :]
+                query_url = query_url.replace(remove_page_url, "")
+
+                return render(
+                    request,
+                    "rooms/search.html",
+                    context={
+                        "form": form,
+                        "rooms": rooms,
+                        "query_url": query_url,
+                    },
+                )
+        else:
+            form = forms.SearchForm()
+
+        return render(
+            request,
+            "rooms/search.html",
+            context={"form": form},
+        )
