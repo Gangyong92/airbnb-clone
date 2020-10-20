b2f1a95ff205d48551806597a2f0e87aa25dad9c
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-20 15:22:54 +0900

    #9 AIRBNB-CLONE ~#14.5
    FormView를 활용한 구현.(LoginView보다 선호)

--- users/views.py
+++ users/views.py
@@ -1,25 +1,26 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "nomadcoders@test.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        # clean method에서 except가 떴으면 유효데이터가 아니라서 False가 뜰거임
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    # form이 유효한지 체크 해줌, 어디론가 보내줄 필요도 다른 걸 할 필요도 없음.
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        # super().form_valid가 호출될 때 success_url로 감.
+        return super().form_valid(form)
 
 
 def log_out(request):
