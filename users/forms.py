0336bbe49c0abd5d24d4b142684d6914b1d907ab
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-20 14:34:41 +0900

    #8 AIRBNB-CLONE ~#14.4
    LoginView 없이 구현

--- users/forms.py
+++ users/forms.py
@@ -8,26 +8,18 @@ class LoginForm(forms.Form):
     # widget=forms.PasswordInput 문자로 출력되지 않도록 옵션 추가
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
-        # 유저가 보낸 데이터에서 email을 찾음
-        email = self.cleaned_data.get("email")
-        try:
-            # 그리고 username에서 email과 같은게 있는지 찾음
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            # 없으면 애러
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
         try:
             # 그리고 username에서 email과 같은게 있는지 찾음
-            user = models.User.objects.get(username=email)
+            user = models.User.objects.get(email=email)
             if user.check_password(password):
-                return password
+                # 필드를 통합한 clean method를 썻다면 무조건 cleaned_data를 써야함
+                return self.cleaned_data
             else:
-                raise forms.ValidationError("Password is wrong")
+                # clean method를 사용할 때는 raise말고 add_error
+                # 로 애러가 뜨는 필드를 지정해줘야함.
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            pass
+            self.add_error("email", forms.ValidationError("User does not exist"))
