9ffb14676c2105910f43348279d9cf78785c6db1
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-22 18:16:45 +0900

    #13 AIRBNB-CLONE ~#17.4
    Github Login 구현

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,8 @@ app_name = "users"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
+    path("login/github", views.github_login, name="github-login"),
+    path("login/github/callback", views.github_callback, name="github-callback"),
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     # key라는 이름으로 str형태로 가져올거임. 그리고 complete_verification에 key로 넘겨줌
