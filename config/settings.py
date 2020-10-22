713855c7d1b338ab9fba1ca8bcece52de4c8c994
Gangyong Hyeong <hyeonggang@naver.com>
2020-10-22 15:44:55 +0900

    #12 AIRBNB-CLONE ~#16.4
    VERIFY EMAIL

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,12 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "tester@sandboxf9cb0f888ef74c548c923681f7ce0732.mailgun.org"
--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
+    # key라는 이름으로 str형태로 가져올거임. 그리고 complete_verification에 key로 넘겨줌
+    path("verify/<str:key>", views.complete_verification, name="complete-verification"),
 ]
