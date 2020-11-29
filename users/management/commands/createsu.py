aa78dae54f04af0a30074091920ec548ec7dcab1
Gangyong Hyeong <hyeonggang@naver.com>
2020-11-29 10:06:59 +0900

    #41 AIRBNB-CLONE ~#26.7

---
+++ users/management/commands/createsu.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    help = "This command creates superuser"
+
+    def handle(self, *args, **options):
+        Users.objects.create_superuser("ebadmin", "hyeonggang@naver.com", "123456")
+        self.stdout.write(self.style.SUCCESS("Superuser Created")
