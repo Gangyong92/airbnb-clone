6862b4b66b4082d6ce840835281533f5d8826db2
Gangyong Hyeong <hyeonggang@naver.com>
2020-11-29 10:52:59 +0900

    #41 AIRBNB-CLONE

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         admin = User.objects.get_or_none(username="ebadmin")
         if not admin:
-            Users.objects.create_superuser("ebadmin", "hyeonggang@naver.com", "123456")
+            User.objects.create_superuser("ebadmin", "hyeonggang@naver.com", "123456")
             self.stdout.write(self.style.SUCCESS("Superuser Created"))
         else:
             self.stdout.write(self.style.SUCCESS("Superuser Exists"))
